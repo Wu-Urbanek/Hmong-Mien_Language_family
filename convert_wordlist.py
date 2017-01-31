04ad62195446257296b5ebd8170a614e1e4fe1c8
MacyL <rorotira@gmail.com>
2017-01-31 18:07:10 +0100

    Update convert_wordlist.py

--- convert_wordlist.py
+++ convert_wordlist.py
@@ -1,17 +1,13 @@
-# librarzy 
+# library
 import pandas as pd 
-import lingpy as lp
-from lingpy import *
 
-
-# read in files 
+# read in the file. 
 x=pd.ExcelFile('hmo_filtered.xls', encoding='ud', index_col=0)
-x.sheet_names
 df=x.parse('Sheet1')
 
 # select columns 
 df_select=df[['Word','Western Xiangxi', 'Eastern Xiangxi', 'Qiandong', 'Chuanqiandian', 'Diandongbei', 'Hmong Daw', 'Hmong Njua', 'Bunu', 'Baonao', 'Numao', 'Longhua Jiongnai', 'Liuxiang Jiongnai', 'Xiaozhai Younuo', 'Huangluo Younuo', 'Northern Pa-Hng', 'Southern Pa-Hng', 'Hm-Nai']]
-
+# transpose the columns to rows
 df_select_transpose=df_select.transpose()
 
 word_list=[]
@@ -24,11 +20,12 @@ for i in df_select_transpose:
 	for j, val in enumerate(ipa_list):
 		word_list.append({'CONCEPT':meaning,'LANGUAGE':location[j], 'IPA':ipa_list[j]})
 
+#get ready for outputing
 word_list_dataframe=pd.DataFrame(word_list)
 word_list_dataframe.index=word_list_dataframe.index+1
 word_list_dataframe=word_list_dataframe[['CONCEPT','LANGUAGE','IPA']]
 word_list_dataframe.to_csv('Hmo_wl.csv',encoding='utf-8',sep='\t')
-
+# The encoding have to be utf-8 or it won't work in Lingpy (my experience)
 
 	
 
