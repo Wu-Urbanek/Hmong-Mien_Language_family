9c5f955447ac18e40ae8c8851f2310cd31d6250d
MeiShinWu <rorotira@gmail.com>
2017-02-27 20:54:11 +0100

    Update PaptoNex.py

--- Nex/PaptoNex.py
+++ Nex/PaptoNex.py
@@ -5,19 +5,14 @@ import sys
 # python3 PaptoNex.py input output 
 #################################
 
-# get the argument 
-Indata=sys.argv[1]
-Outname=sys.argv[2]
-
 # load the hand manual alignment file as wordlist object
-wl=Wordlist(Indata)
+wl=Wordlist(sys.argv[1])
 
-# this will get the dictionary which cogid is key and the index of the row is value 
-wl_paps=wl.get_paps(ref="COGID",entry='concept',missing="?",modify_ref=False)
+# this will get the dictionary which cogid is key and the index of the row is value
+wl_paps=wl.get_paps(ref="COGIDS",entry='CONCEPT',missing="?",modify_ref=False)
 
-# create taxa, check the "get_entries" first 
-wl_entries=wl.get_entries("DOCULECT") # this one has full taxa 
-wl_taxa=wl_entries[2]# this one has full taxa 
+# create taxa
+wl_taxa=wl.language
 
 # convert to nex
- lingpy.convert.strings.pap2nex(wl_taxa, wl_paps, missing='?', filename=Outname)
+lingpy.convert.strings.pap2nex(wl_taxa, wl_paps, missing='?', filename=sys.argv[2])
