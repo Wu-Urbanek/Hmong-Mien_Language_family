1f9770654dd81d3a34131ac2090f9198a087480a
MeiShinWu <MacyL@users.noreply.github.com>
2017-03-15 13:54:32 +0100

    Update compare.py

--- Python_Script/compare.py
+++ Python_Script/compare.py
@@ -20,3 +20,23 @@ WLap=Wordlist('HM-March4-partial-final.tsv')
 WLmp_bcube=bcubes(WLmp, 'cogid','cogids')
 WLap_bcube=bcubes(WLap,'cogid','cogids')
 
+###################
+# Not sure if this make sence, but just trying out at this moment 
+# get edit distance for every concept
+###################
+Lan=WLmp.language
+
+lanD=dict()
+for i in Lan:
+	temp=WLmp.get_list(language=i, entry='SEGMENTS_IS')
+	lanD[i]=temp
+
+lanpair=list(permutations(Lan,2))
+
+lanD_editD=dict()
+
+for k,v in lanpair:
+	seqA=['-' if x==0 else x for x in lanD[k]]
+	seqB=['-' if x==0 else x for x in lanD[v]]
+	temp=[edit_dist(seqA[i],seqB[i]) for i in range(0,len(seqA))]
+	lanD_editD[(k,v)]=temp
